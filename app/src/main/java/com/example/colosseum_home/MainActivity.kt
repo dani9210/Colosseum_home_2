63bf44ad0c25fe8e4229f63111d2f675894945b7
김다은 <softeun92@naver.com>
2021-11-08 22:07:18 +0900

    로그인화면

--- app/src/main/java/com/example/colosseum_home/MainActivity.kt
+++ app/src/main/java/com/example/colosseum_home/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.colosseum_home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.example.colosseum_home.databinding.ActivityMainBinding
 
@@ -14,5 +15,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+
+
+        binding.loginBtn.setOnClickListener {
+
+            val inputEmail = binding.emailEdt.text.toString()
+            val inputPw = binding.passwordEdt.text.toString()
+
+            Log.d("입력이메일",inputEmail)
+            Log.d("입력비밀번호",inputPw)
+
+
+
+        }
+
+
+
     }
 }
