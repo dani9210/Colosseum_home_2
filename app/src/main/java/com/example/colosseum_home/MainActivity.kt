647514d748ddbf8de5cc7a1d42db1184718dea6c
김다은 <softeun92@naver.com>
2021-11-12 16:47:12 +0900

    화면단에서, 서버에 다녀오면 어떻게 할지 응답 처리방안 코딩.   - runOnUiThread {  }로, 백그라운드가 Ui를건드리는 행위를 방지

--- app/src/main/java/com/example/colosseum_home/MainActivity.kt
+++ app/src/main/java/com/example/colosseum_home/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.colosseum_home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.colosseum_home.databinding.ActivityMainBinding
 import com.example.colosseum_home.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,7 +31,50 @@ class MainActivity : AppCompatActivity() {
 
 //            서버의 로그인 기능에 전달.
 
-            ServerUtil.postRequestLogin(inputEmail,inputPw)
+            ServerUtil.postRequestLogin(inputEmail,inputPw,object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+
+//                    화면단에서 jsonObj 분석- > 상황에맞는 UI처리
+
+                    val code = jsonObj.getInt("code")
+//                    로그인 성공시 - > 성공
+//                   실패시 -> 왜 실패했는지 서버가 알려주는대로 토스트
+
+                    runOnUiThread {
+
+
+                        if (code==200){
+
+
+
+
+                            Toast.makeText(this@MainActivity, "로그인 성공", Toast.LENGTH_SHORT).show()
+
+                        }
+
+                        else{
+
+                            val message = jsonObj.getString("message")
+
+                            Toast.makeText(this@MainActivity, "${message}", Toast.LENGTH_SHORT).show()
+
+
+
+
+                        }
+
+
+
+
+
+                    }
+
+
+
+                }
+
+
+            })
 
 
 
