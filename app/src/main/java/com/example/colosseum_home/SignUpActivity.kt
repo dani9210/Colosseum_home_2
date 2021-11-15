e139754e4d785dba854be52bb4813ba56ae134e3
김다은 <softeun92@naver.com>
2021-11-15 11:34:05 +0900

    회원가입 화면 추가   -  데이터 바인딩 세팅

---
+++ app/src/main/java/com/example/colosseum_home/SignUpActivity.kt
@@ -0,0 +1,21 @@
+package com.example.colosseum_home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.colosseum_home.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+
+
+    lateinit var binding : ActivitySignUpBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_sign_up)
+
+
+    }
+
+
+}
