b49eb60c0781ac47edea3cff7e21e0044bce041e
김다은 <softeun92@naver.com>
2021-11-15 17:03:46 +0900

    회원가입하는 API 호출 함수 구현   - ServerUtil 기능을 추가

--- app/src/main/java/com/example/colosseum_home/SignUpActivity.kt
+++ app/src/main/java/com/example/colosseum_home/SignUpActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.colosseum_home.databinding.ActivitySignUpBinding
+import com.example.colosseum_home.utils.ServerUtil
+import org.json.JSONObject
 
 class SignUpActivity :BaseActivity() {
 
@@ -29,6 +31,20 @@ class SignUpActivity :BaseActivity() {
             val inputNickname = binding.nicknameEdt.text.toString()
 
 //            입력한 데이터를 => 서버의 회원가입 기능에 요청  => ServerUtil 함수 활용. => 함수가 아직없으니 추가로 만들자
+
+            ServerUtil.putRequestSignUp(inputEmail,inputPw,inputNickname,object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+
+//                     jsonObj 분석 -> UI반영 코드만 작성.
+
+
+
+
+
+                }
+
+
+            })
 
 
 
