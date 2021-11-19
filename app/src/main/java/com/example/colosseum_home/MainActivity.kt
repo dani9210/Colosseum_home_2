02e5e2ec1a7c17bc6c4c9779f9490a9dd0ae00c1
김다은 <softeun92@naver.com>
2021-11-19 15:08:25 +0900

    메인화면 정볼르 서버에 요청하는 API 추가   - 파싱작업 X

--- app/src/main/java/com/example/colosseum_home/MainActivity.kt
+++ app/src/main/java/com/example/colosseum_home/MainActivity.kt
@@ -4,12 +4,15 @@ package com.example.colosseum_home
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.colosseum_home.databinding.ActivityMainBinding
+import com.example.colosseum_home.datas.TopicData
 import com.example.colosseum_home.utils.ServerUtil
 import org.json.JSONObject
 
 class MainActivity : BaseActivity(){
 
     lateinit var binding : ActivityMainBinding
+
+    val mTopicList = ArrayList<TopicData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,10 +29,26 @@ class MainActivity : BaseActivity(){
 
     override fun setValues() {
 
+//        /v2/main_info API가 토론 주제 목록을 내려줌.
+//        서버 호출 =>  파싱해서, mTopicList를 채워주자.
+        getTopicListFromServer()
+
+    }
+
+
+
+    fun getTopicListFromServer() {
+
+        ServerUtil.getRequestMainInfo(mContext,object : ServerUtil.JsonResponseHandler{
+            override fun onResponse(jsonObj: JSONObject) {
 
 
 
 
+            }
+
+
+        })
 
 
     }
