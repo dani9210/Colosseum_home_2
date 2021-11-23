638c6f5c33170e6fd9c162809d5240a0a8dcb3bd
김다은 <softeun92@naver.com>
2021-11-23 14:14:41 +0900

    토론상세 댓글 목록 파싱   - 댓글목록  ArrayList에 추가

--- app/src/main/java/com/example/colosseum_home/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/example/colosseum_home/ViewTopicDetailActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.example.colosseum_home.databinding.ActivityViewTopicDetailBinding
+import com.example.colosseum_home.datas.ReplyData
 import com.example.colosseum_home.datas.TopicData
 import com.example.colosseum_home.utils.ServerUtil
 import org.json.JSONObject
@@ -15,6 +16,7 @@ class ViewTopicDetailActivity : BaseActivity() {
 
     lateinit var mTopicData : TopicData
 
+    val mReplyList = ArrayList<ReplyData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +45,35 @@ class ViewTopicDetailActivity : BaseActivity() {
 
         ServerUtil.getRequestTopicDetail(mContext,mTopicData.id,"NEW",object : ServerUtil.JsonResponseHandler{
             override fun onResponse(jsonObj: JSONObject) {
+
+//                댓글 목록 JSONArray -> 파싱 -> mReplyList의 자료로 추가
+
+                val dataObj  = jsonObj.getJSONObject("data")
+                val topicObj = dataObj.getJSONObject("topic")
+                val repliesArr = topicObj.getJSONArray("replies")
+
+                for( i in 0 until repliesArr.length()) {
+
+                    val replyObj = repliesArr.getJSONObject(i)
+
+//                    JSONObjec -> ReplyData 객체로 벼환.
+
+                    val replyData = ReplyData()
+                    replyData.id = replyObj.getInt("id")
+                    replyData.content = replyObj.getString("content")
+
+//                    댓글 목록으로 추가
+
+                    mReplyList.add(replyData)
+
+                }
+
+//                리스트뷰의 목록에 변경  => 리스트뷰 어댑터 새로고침 처리
+
+
+
+
+
 
 
 
