d7645ee383b822ed88a2dee9f0f03bdcb08acafb
김다은 <softeun92@naver.com>
2021-11-24 14:37:59 +0900

    선택 진영 데이터 파싱 함수 추가   - 진영 파싱은 이 함수 이용 예정

--- app/src/main/java/com/example/colosseum_home/datas/SideData.kt
+++ app/src/main/java/com/example/colosseum_home/datas/SideData.kt
@@ -1,4 +1,28 @@
 package com.example.colosseum_home.datas
 
+import org.json.JSONObject
+
 class SideData {
+
+    var id = 0
+    var title = ""
+    var voteCount = 0
+
+    companion object{
+
+        fun getSideDataFromJSON(jsonObj : JSONObject) : SideData {
+
+            val sideData = SideData()
+
+            sideData.id = jsonObj.getInt("id")
+            sideData.title = jsonObj.getString("title")
+            sideData.voteCount = jsonObj.getInt("vote_count")
+
+            return sideData
+
+        }
+
+
+    }
+
 }
